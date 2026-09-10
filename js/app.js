1f33750e29b0d0c6dea3b66c8a1383c108c09a3d
army99112-source <army99112@gmail.com>
2026-09-10 14:32:50 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const courses = [
   { title: "Class 8", class: "8th Standard", subject: "All Subjects", color: "#8b5cf6" },
   { title: "Class 9", class: "9th Standard", subject: "All Subjects", color: "#ef4444" },
   { title: "Class 10", class: "10th Standard", subject: "All Subjects", color: "#f59e0b" },
-  { title: "PINTISIR", class: "Class 11", subject: "All Subjects", color: "#6366f1" },
+  { title: "PINTU SIR", class: "Class 11", subject: "All Subjects", color: "#6366f1" },
   { title: "DEEPAK SIR", class: "Class 12", subject: "All Subjects", color: "#22c55e" }
 ];
 
