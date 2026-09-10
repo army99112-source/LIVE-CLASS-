0f4ad8e9a787a8e2f185dba59554ac5a13079bd1
army99112-source <army99112@gmail.com>
2026-09-10 10:41:21 +0530

    Add Firebase configuration file

---
+++ js/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "YAHAN_API_KEY",
+  authDomain: "YAHAN_AUTH_DOMAIN",
+  projectId: "YAHAN_PROJECT_ID",
+  storageBucket: "YAHAN_STORAGE_BUCKET",
+  messagingSenderId: "YAHAN_SENDER_ID",
+  appId: "YAHAN_APP_ID"
+};
